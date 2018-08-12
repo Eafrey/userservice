b5a1ba9b01d2762576c4b9901b027ff0b69ca6cd
Eafrey <1019812436@qq.com>
2018-08-12 13:19:47 +0800

    docker swarm first

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./gradlew clean bootRepackage
+
+docker build --rm . --tag chensen/user-service:${VER:?invalid version}
+docker push chensen/user-service:${VER:?invalid version}
+
+export VER
+docker stack deploy todo -c docker-compose.yml
