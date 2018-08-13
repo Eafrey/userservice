ec01d59c25a0f609373b0df6a14c2f75b5c3a3f3
Eafrey <1019812436@qq.com>
2018-08-13 21:34:24 +0800

    ok in local proper

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ./gradlew clean bootRepackage
 
 docker build --rm . --tag chensen/user-service:${VER:?invalid version}
-docker push chensen/user-service:${VER:?invalid version}
+#docker push chensen/user-service:${VER:?invalid version}
 
 export VER
-docker stack deploy todo -c docker-compose.yml
+#docker stack deploy todo -c docker-compose.yml
